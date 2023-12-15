17dcdb3efa7c24ac52dfd97d0a9ce03b102f8843
hasan <hasancilenger1773@gmail.com>
2023-12-15 15:48:18 +0100

    tutorial

---
+++ react/class-notes/rc10_tutorial-app/tutorial-app-inClass/src/components/TutorialList.jsx
@@ -0,0 +1,86 @@
+import { FaEdit } from "react-icons/fa"
+import { AiFillDelete } from "react-icons/ai"
+import axios from "axios"
+import EditTutorial from "./EditTutorial"
+import { useState } from "react"
+
+const TutorialList = ({ tutorials, getTutorials }) => {
+  const [editData, setEditData] = useState("")
+  //? mock data
+  // const tutorials = [
+  //   {
+  //     id: 1,
+  //     title: "JS",
+  //     description: "JS is a programming language",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "React",
+  //     description: "JS library for UI design",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Solid.JS",
+  //     description: "JS library for UI design",
+  //   },
+  // ]
+
+  const deleteTutorial = async (id) => {
+    try {
+      await axios.delete(`${process.env.REACT_APP_URL}${id}/`)
+    } catch (error) {
+      console.log(error)
+    }
+    getTutorials()
+  }
+
+  console.log(editData)
+
+  return (
+    <div className="container mt-4">
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#id</th>
+            <th scope="col">Title</th>
+            <th scope="col">Description</th>
+            <th scope="col" className="text-center">
+              Edit
+            </th>
+          </tr>
+        </thead>
+        <tbody>
+          {tutorials?.map((item) => {
+            const { id, title, description } = item
+            return (
+              <tr key={id}>
+                <th>{id}</th>
+                <td>{title}</td>
+                <td>{description}</td>
+                <td className="text-center text-nowrap">
+                  <FaEdit
+                    size={20}
+                    type="button"
+                    className="me-2 text-warning"
+                    data-bs-toggle="modal"
+                    data-bs-target="#open-modal"
+                    onClick={() => setEditData(item)}
+                  />
+                  <AiFillDelete
+                    size={22}
+                    type="button"
+                    className="text-danger "
+                    onClick={() => deleteTutorial(id)}
+                  />
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <EditTutorial editData={editData} />
+    </div>
+  )
+}
+
+export default TutorialList
---
+++ react/class-notes/rc10_tutorial-app/tutorial-app-inClass/src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react"
+import AddTutorial from "../components/AddTutorial"
+import TutorialList from "../components/TutorialList"
+import axios from "axios"
+
+const Home = () => {
+  const [tutorials, setTutorials] = useState([])
+
+  const getTutorials = async () => {
+    try {
+      // const URL = "https://tutorial-api.fullstack.clarusway.com/tutorials/"
+      const res = await axios(process.env.REACT_APP_URL)
+      setTutorials(res.data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  //? Mount asamasinda istek atilmali
+  useEffect(() => {
+    getTutorials()
+  }, [])
+
+  console.log(tutorials)
+  return (
+    <>
+      <AddTutorial getTutorials={getTutorials} />
+      <TutorialList tutorials={tutorials} getTutorials={getTutorials} />
+    </>
+  )
+}
+
+export default Home

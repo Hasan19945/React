ad6528fe4277829e4d8f3b6aee87d39f89991eaa
hasan <hasancilenger1773@gmail.com>
2023-12-28 22:18:30 +0100

    router

--- react/class-notes/rc17-18_movie-app/movie-app-inclass/src/App.js
+++ react/class-notes/rc17-18_movie-app/movie-app-inclass/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import AppRouter from "./router/AppRouter";
 import AuthContextProvider from "./context/AuthContext";
+import { ToastContainer } from "react-toastify";
+
 
 const App = () => {
   return (
-    <div>
+    <div className="dark:bg-gray-dark-main min-h-screen">
       <AuthContextProvider>
-        <AppRouter />
+        
+          <AppRouter />
+          <ToastContainer />
+        
       </AuthContextProvider>
     </div>
   );

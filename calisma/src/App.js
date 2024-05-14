226f51d497146c6a06ee0812d0d3384f59b88623
hasan <hasancilenger1773@gmail.com>
2024-05-14 08:59:43 +0200

    useState

---
+++ calisma/src/App.js
@@ -0,0 +1,28 @@
+
+import {useState} from 'react';
+function App () {
+  const [count, setCount] = useState(0);
+  const [hesap , setHesap] = useState(4);
+  function handleClick() {
+    setCount(count + 1);
+    setHesap(hesap+3);
+  }
+
+  function Bas () {
+    setHesap(hesap +5);
+  }
+  return (
+    <>
+    <button onClick={handleClick}>
+      you presset here {count} times
+    </button>
+    
+    <button onClick={Bas}>
+
+      {hesap} kere bastin
+    </button>
+    </>
+  )
+}
+
+export default App;
